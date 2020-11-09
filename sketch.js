2ac7ea605eb15242eb16bf9c96e672611ad9d5d2
Shaun-Jose <73694093+Shaun-Jose@users.noreply.github.com>
2020-11-09 14:47:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(800,400);
+  movingrectangle=createSprite(400, 200, 80, 30);
+  fixedrectangle=createSprite(200,200,50,80)
+  movingrectangle.shapeColor='green'
+  fixedrectangle.shapeColor='green'
+  movingrectangle.debug=true;
+  fixedrectangle.debug=true;
+}
+
+function draw() {
+  background('black');  
+  movingrectangle.x=World.mouseX
+  movingrectangle.y=World.mouseY
+  if(movingrectangle.x-fixedrectangle.x<movingrectangle.width/2+fixedrectangle.width/2&&
+    fixedrectangle.x-movingrectangle.x<movingrectangle.width/2+fixedrectangle.width/2&&
+    movingrectangle.y-fixedrectangle.y<movingrectangle.height/2+fixedrectangle.height/2&&
+    fixedrectangle.y-movingrectangle.y<movingrectangle.height/2+fixedrectangle.height/2){
+    movingrectangle.shapeColor='red'
+    fixedrectangle.shapeColor='red'
+  }
+  else{movingrectangle.shapeColor='green'
+       fixedrectangle.shapeColor='green'
+}
+  drawSprites();
+}
